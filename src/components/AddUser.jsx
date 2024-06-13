fc737e12852cfda2c04cc8062b8c7b07f37cb503
ebinthomas01 <ebintkm@gmail.com>
2024-06-13 14:29:28 +0530

    Add user UI edited

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -24,42 +24,12 @@ const AddUser = () => {
                   <label htmlFor="" className="label fomr-label">Mobile</label>
                   <input type="text" className="form-control"  />
                   </div>
-                  <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                  <label htmlFor="" className="label fomr-label">Address</label>
-                  <textarea name="address" id="" className="form-control"  ></textarea>
-                  </div>
                   
-                  <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                  <label htmlFor="" className="label form-label">District</label>
-                  <select name="blood" id="" className="form-control">
-                    <option value="	Alappuzha">	Alappuzha</option>
-                    <option value="Ernakulam">Ernakulam</option>
-                    <option value="Idukki">Idukki</option>
-                    <option value="Kannur">Kannur</option>
-                    <option value="Kasaragod">	Kasaragod</option>
-                    <option value="Kollam">Kollam</option>
-                    <option value="Kottayam">Kottayam</option>
-                    <option value="Kozhikode">Kozhikode</option>
-                    <option value="Malappuram">Malappuram</option>
-                    <option value="Palakkad">Palakkad</option>
-                    <option value="Pathanamthitta">Pathanamthitta</option>
-                    <option value="Thiruvananthapuram">Thiruvananthapuram</option>
-                    <option value="Thrissur">Thrissur</option>
-                    <option value="Wayanad">Wayanad</option>
-                  </select>
-                  </div>
-                  <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                  <label htmlFor="" className="label fomr-label">Place</label>
-                  <input type="text" className="form-control" name='place'  />
-                  </div>
                   <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                   <label htmlFor="" className="label fomr-label">Email</label>
                   <input type="text" className="form-control" name='email'  />
                   </div>
-                  <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                  <label htmlFor="" className="label fomr-label">User Name</label>
-                  <input type="text" className="form-control" name='username' />
-                  </div>
+                
                   <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                   <label htmlFor="" className="label fomr-label">Password</label>
                   <input type="text" className="form-control" name='password'  />
