eb137a9b6b727a910c9b663214e9747a45ad5373
ebinthomas01 <ebintkm@gmail.com>
2024-06-13 17:06:54 +0530

    frontend completed

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -2,7 +2,39 @@ import axios from 'axios'
 import React, { useState } from 'react'
 
 const AddUser = () => {
-    
+        
+    const [data, setData] = useState(
+        {
+            "name":"",
+            "email":"",
+            "password":""
+            
+        }
+    )
+    const inputHandler = (event) => {
+        setData({ ...data, [event.target.name]: event.target.value })
+    }
+    const readValue = () => {
+        console.log(data)
+        axios.post("http://localhost:8080/signup", data).then(
+            (response) => {
+                console.log(response.data)
+                if (response.data.status == "success") 
+                    {
+                    alert("Successfully Registered")
+                    }
+                else {
+                    alert("Error")
+                }
+            }
+
+        ).catch(
+            (error) => {
+                console.log(error.message)
+                alert(error.message)
+            }
+        )
+    }
     
   return (
     <div>
@@ -12,35 +44,21 @@ const AddUser = () => {
                 <div className="row g-3">
                   <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <label htmlFor="" className="label fomr-label">Name</label>
-                    <input type="text" className="form-control"  />
-                  </div>
-
-                  <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                  <label htmlFor="" className="label fomr-label">Date Of Birth</label>
-                  <input type="date" className="form-control" />
-                  </div>
-                  
-                  <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                  <label htmlFor="" className="label fomr-label">Mobile</label>
-                  <input type="text" className="form-control"  />
+                    <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler} />
                   </div>
                   
                   <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                   <label htmlFor="" className="label fomr-label">Email</label>
-                  <input type="text" className="form-control" name='email'  />
+                  <input type="text" className="form-control" name='email' value={data.email} onChange={inputHandler} />
                   </div>
                 
                   <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                   <label htmlFor="" className="label fomr-label">Password</label>
-                  <input type="text" className="form-control" name='password'  />
-                  </div>
-                  <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                  <label htmlFor="" className="label fomr-label">Confirm Password</label>
-                  <input type="text" className="form-control" name='confirmpass' />
+                  <input type="password" className="form-control" name='password' value={data.password} onChange={inputHandler} />
                   </div>
                   
-                  <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                    <button className="btn btn-warning"  > CREATE ACCOUNT</button>
+                  <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <button className="btn btn-warning" onClick={readValue} > CREATE ACCOUNT</button>
                   </div>
                   <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                   <a href='/'> Go Home</a>
